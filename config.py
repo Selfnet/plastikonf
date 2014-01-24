578bbe0bff0402ac029be43c00169c19358ce4e8
Lukas Kramer <lukas@carrotIndustries.net>
2014-01-24 23:13:19 +0100

    speak with me

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 iface = "wlp2s0"
 netctl_profile = "plastikonf"
 printer_ip = "10.0.240.23"
+speech_enable = True
--- insert.py
+++ insert.py
@@ -1,24 +1,39 @@
 import randomcity
 import tempfile
-import subprocess
 import os
 import pymongo
 import time
+import config
+
+if config.speech_enable :
+	import speechd
+	speaker = speechd.Speaker("plastikonf")
+	speaker.set_output_module("espeak")
+
+def say(m) :
+	if config.speech_enable :
+		speaker.speak(m)
 
 c = pymongo.MongoClient()
 db = c.plastikonf
 devices_coll = db.devices
 
+
+
 while True :
+	say("enter MAC")
 	mac=input("MAC: ").strip().replace(":", "").upper()
 	if len(tuple(devices_coll.find({"mac": mac}))) > 0 :
+		say("duplicate MAC")
 		print("duplicate MAC")
 		continue
 	if len(mac) != 12 or not all(map(lambda z: z.isdigit() or z in "ABCDEF", mac)):
+		say("invalid MAC")
 		print("invalid MAC")
 		continue
-	pin=input("PSK: ").strip()
+	pin=input("PIN: ").strip()
 	if len(pin) != 8 or not all(map(str.isdigit, pin)) :
+		say("invalid PIN")
 		print("invalid PIN")
 		continue
 
@@ -32,6 +47,7 @@ while True :
 		print("ESSID: "+essid)
 		print("WPA  : "+wpakey)
 		print("ADMIN: "+adminpw)
+		say("please confirm")
 		c=input("[Y]es, [n]ew, [e]dit: ").lower()
 		if c=="y" or c=="" :
 			break
